3de0f0a3035ec7cfd5c261df6fa8ea76de6b3dbd
Aqsanoor02 <154265203+Aqsanoor02@users.noreply.github.com>
2024-09-16 19:18:40 +0500

    Add files via upload

---
+++ M1-2/script.ts
@@ -0,0 +1,10 @@
+let button=document.getElementById('button') as HTMLButtonElement;
+const skills=document.getElementById('skills') as HTMLDivElement;
+
+button.addEventListener('click',()=>{
+    if(skills.style.display==='none'){
+        skills.style.display='block'
+    }else{
+        skills.style.display='none'
+    }
+});
